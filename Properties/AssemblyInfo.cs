8132dbe29a38057bafa80ce65701fda6a4768385
Ricky Bennett <norman.bennett525@gmail.com>
2021-01-27 09:31:47 -0800

    Morning Challenge 1/27/2020

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MorningChallengeW1D3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MorningChallengeW1D3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12f19c55-fc0d-4391-8056-6c731946d146")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ super.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MorningChallengeW1D3
+{
+    [TestClass]
+    public class super
+    {
+        [TestMethod]
+        public void SuperLoop()
+        {
+            string word = "Supercalifragilisticexpialidocious";
+
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
+
+
